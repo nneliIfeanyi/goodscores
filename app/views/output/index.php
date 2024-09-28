1f78b328788fd2b2403de59331c2f4022280872a
Stanvic <stanvicbest@gmail.com>
2024-09-28 11:55:53 +0100

    lets see

--- app/views/output/index.php
+++ app/views/output/index.php
@@ -267,6 +267,37 @@ foreach ($data['theory'] as $theory) {
 </table>';
 
 
+if (!empty($pull_each->questionC)) {
+		$table .= '<table>
+		<tr>
+					<td width="25"><b>' . $num2 . 'c)</b></td>
+					<td style="width:667px;">' . $pull_each->questionC . '<br>';
+	}
+
+
+	if (!empty($pull_each->Ci)) {
+		$table .= '
+					<span><b>(i)&nbsp;</b>' . $pull_each->Ci . '</span>
+						
+		';
+	}
+	if (!empty($pull_each->Cii)) {
+		$table .= '
+					<span><b>(ii)&nbsp;</b>' . $pull_each->Cii . '</span>
+						
+		';
+	}
+	if (!empty($pull_each->Ciii)) {
+		$table .= '
+					<span><b>(iii)&nbsp;</b>' . $pull_each->Ciii . '</span>
+						
+		';
+	}
+	$table .= '</td>
+	</tr>
+</table>';
+
+
 
 	if (!empty($pull_each->questionD)) {
 		$table .= '<table>
