4c906677fb92813a605b264ff3336ef1c88856e5
Stanvic <stanvicbest@gmail.com>
2024-10-11 15:40:48 +0100

    rm

--- app/views/posts/custom.php
+++ app/views/posts/custom.php
@@ -91,7 +91,14 @@
 </main><!-- End #main -->
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
-
+<!-- Page loader fade in on form submit -->
+<script>
+  $(':submit').each(function() {
+    $(this).click(function() {
+      $('#loader').fadeIn();
+    });
+  });
+</script>
 <script>
   tinymce.init({
     selector: 'textarea',
