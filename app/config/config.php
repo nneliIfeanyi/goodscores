820200305d1a8fa518f052ff120e98e64896cba6
nneliifeanyi <stanvicbest@gmail.com>
2026-03-01 23:12:16 +0100

    deployed v2

--- app/config/config.php
+++ app/config/config.php
@@ -13,8 +13,8 @@
 // // Site Name
 // define('SITENAME', 'GoodScores');
 define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_concepts");
-define("DB_PASS", "Ebenezer@30");
+define("DB_USER", "stanvicc_Avalanche");
+define("DB_PASS", "Avalanche@25");
 define("DB_NAME", "stanvicc_goodscores");
 
 // App Root
